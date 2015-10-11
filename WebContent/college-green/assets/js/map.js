5d31c6efde96944a2c39c59aab40dba8d5aba813
lshastry <lshastry@yahoo-inc.com>
2015-10-11 14:17:39 -0700

    Added new code base consistent with website

--- WebContent/college-green/assets/js/map.js
+++ WebContent/college-green/assets/js/map.js
@@ -3,15 +3,15 @@ jQuery(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 17.321448,
+        lng: 78.520368,
     });
     map.addMarker({
         lat: 51.451573,
         lng: -2.595008,
         title: 'Address',      
         infoWindow: {
-            content: '<h5 class="title">College Green Campus</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
+            content: '<h5 class="title">Vishra Academy</h5><p><span class="street-address">K.Srinivas Reddy Complex</span><span class="region">Near Balapur X Roads, Meerpet</span><br><span class="postal-code">500097</span><br><span class="state-name">Telangana</span><br><span class="country-name">India</span></p>'
         }
         
     });
